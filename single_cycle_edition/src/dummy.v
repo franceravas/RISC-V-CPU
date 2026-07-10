4b58a7357ee5916932fc845375d8224006561893
franceravas <france.ravas@yahoo.com>
2026-07-10 13:10:37 -0600

    Initial commit

--- single_cycle_edition/src/dummy.v
+++ single_cycle_edition/src/dummy.v
@@ -1,8 +1,16 @@
-module dummy (
-    input clk,
-    output reg [7:0] count
+module logic1 (
+    input logic clk,
+    input logic reset_n,
+    input logic data_in,
+    output logic data_out
 );
-    always @(posedge clk) begin
-        count <= count + 1;
+
+always @(posedge clk) begin
+    if(~reset_n) begin
+        data_out <= 1'b0;
+    end else begin
+        data_out <= data_in;
     end
+end
+    
 endmodule
